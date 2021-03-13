eb8b9b7f8a53dba9639d495d4831c6903c738221
zkdev <dev@zeekay.dev>
2021-03-13 13:08:29 +0100

    Add solo logic

---
+++ core/api.py
@@ -0,0 +1,63 @@
+import logging
+import requests
+
+import falcon
+
+import common.util
+
+
+logger = common.util.Logger(__name__)
+
+
+CRAWLER_ENDPOINT = 'https://crawler.run-it-down.lol/summoner'
+REPORT_ENDPOINT = 'https://crawler.run-it-down.lol/report'
+
+
+class Solo:
+
+    def on_post(self, req, resp):
+        logger.info('POST /analyze/solo')
+        body = json.loads(req.stream.read())
+
+        # surpress tls check
+        urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+        # execute crawler
+        # TODO remove hardcoded range
+        data = {
+            'summonerName': body['summonerName'],
+            'startIndex': '0',
+            'endIndex': '10'
+        }
+        header = {
+            'X-Riot-Token': body['XRiotToken'],
+            'endpoint': 'https://euw1.api.riotgames.com/'
+        }
+        logger.info('calling crawler')
+        res = requests.post(url=CRAWLER_ENDPOINT, data=data, header=header)
+
+        # get report
+        if res.status_code == 201:
+            params = {
+                'summoner': body['summonerName']
+            }
+            report = requests.get(url=common.util.urljoin(REPORT_ENDPOINT, '/solo'), params=params)
+        else:
+            logger.error('calling crawler failed')
+
+        # return report
+        if report.status_code == 201:
+            resp.body = report.content
+            resp.status_code = 201
+        else:
+            logger.error('no report returned')
+
+
+def create():
+    api = falcon.API()
+    api.add_route('/analyze/solo', Solo())
+    logger.info('falcon initialized')
+    return api
+
+
+application = create()
